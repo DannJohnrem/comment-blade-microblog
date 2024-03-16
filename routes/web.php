e827a52baa001d287b21891018c47066742fd1ec
DannJohnrem <jonrhem10@gmail.com>
2024-03-17 01:43:58 +0800

    add a resource class in the route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -26,6 +27,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Chirp route
+    Route::resource('chirps', [ChirpController::class])->only('index', 'store');
 });
 
 require __DIR__.'/auth.php';
