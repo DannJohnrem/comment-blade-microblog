f0b0857f1d7559004126b07c1b19cae2468d68bd
DannJohnrem <jonrhem10@gmail.com>
2024-03-18 23:05:28 +0800

    fix the routes

--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    // Chirp route
-    Route::resource('chirps', [ChirpController::class])->only('index', 'store');
+    Route::resource('chirps', ChirpController::class)->only(['index', 'store']);
 });
+
+
 
 require __DIR__.'/auth.php';
