e320c6034e2ba718ecba71a0a5d82a635e1aa914
DannJohnrem <jonrhem10@gmail.com>
2024-03-19 21:48:30 +0800

    add navigation menu

--- resources/views/pages/chirps/index.blade.php
+++ resources/views/pages/chirps/index.blade.php
@@ -4,7 +4,7 @@
             @csrf
 
             <textarea name="message" id="Message" placeholder="{{ __('What\'s on your mind?') }}"
-                class="block w-full border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ old('message') }}</textarea>
+                class="block w-full border-gray-500 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">{{ old('message') }}</textarea>
             <x-input-error :messages="$errors->get('message')" class="mt-2"/>
             <x-primary-button class="mt-4">{{ __('Send') }}</x-primary-button>
         </form>
